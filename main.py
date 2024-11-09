659a60ac1c92d114f6c23f0455c0c160b0db1de4
Lucifer Akirami <110820999+LuciAkirami@users.noreply.github.com>
2024-11-09 13:13:03 +0000

    added basic ai service

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
---
+++ services/prompts.py
@@ -0,0 +1,9 @@
+USER_PROMPT = """
+Answer the user question based on the context
+
+CONTEXT:
+{context}
+
+USER_QUESTION:
+{query}
+"""
